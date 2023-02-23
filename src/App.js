b8564014c2d1080dba4ade939ebe8981ab83eeab
nanthu1696 <118247818+nanthu1696@users.noreply.github.com>
2023-02-23 18:08:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,34 +2,56 @@ import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import Element from './components/element';
-
+import {useState} from 'react';
 function App() {
+  let [arr, setArr] = useState([]);
+  let [val, setVal]  = useState("");
+  let [popu, setPopu] = useState("");
+  let [topu, setTopu] = useState("");
+  function handleArr()
+  {
+    let arr2 = [val,...arr]
+    setArr(arr2);
+  }
+  function handle()
+  {
+    setPopu(arr.shift());
+
+  }
+  function handletop()
+  {
+    let l = arr.length;
+    let arr2 = arr;
+    setTopu(arr2[5]);
+  }
   return (
     <div className="container">
       <h1 className="header">Stack</h1>
       <div className="row">
         <div className="col md-6 stack">
-          <Element />
+          {arr.map((each,index)=>(
+            <Element  valu = {each}/>
+          ))}
         </div>
         <div className="col md-3 operation">
           <h2>Operations</h2>
           <div className="input-group push">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+            <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick = {handleArr}>
               PUSH
             </button>
-            <input type="text" className="form-control" />
+            <input type="text" className="form-control" onChange = {(event)=> setVal(event.target.value)}/>
           </div>
           <div className="input-group pop">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+            <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={handle}>
               POP
             </button>
-            <input type="text" disabled className="form-control" />
+            <input type="text" disabled className="form-control" value = {popu} />
           </div>
           <div className="input-group top">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+            <button className="btn btn-outline-secondary" type="button" id="button-addon2"onClick = {handletop}>
               TOP
             </button>
-            <input type="text" disabled className="form-control" />
+            <input type="text" disabled className="form-control" value = {topu} />
           </div>
         </div>
       </div>
